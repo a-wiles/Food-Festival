d91d6763871e1bd306d0fac553816d0bd774955f
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-06-06 23:58:15 -0400

    Fixed js scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,13 @@ const path = require("path");
 
 
 
-module.exports = { entry: './assets/js/script.js' };
+module.exports = {
+    entry: './assets/js/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
+    },
+
+    mode: 'development'
+};
+
